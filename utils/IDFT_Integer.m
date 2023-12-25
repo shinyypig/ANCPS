8781198af6f27cb43a4cd91fadce37b4c8b744fd
Liangliang Zhu <shinyypig@gmail.com>
2023-12-25 15:32:33 +0800

    Fix bug in IDFT_US

--- utils/IDFT_Integer.m
+++ utils/IDFT_Integer.m
@@ -1,13 +1,13 @@
 function [m0, n0] = IDFT_Integer(im1, im2)
-%the IDFT based translation matching algorithm
-%   only return the integer matching results
+    %the IDFT based translation matching algorithm
+    %   only return the integer matching results
 
     [m, n] = size(im1);
     I1 = fft2((im2));
     I2 = fft2((im1));
     I = I1 .* conj(I2);
     I = I ./ abs(I);
-    R = abs(fftshift(ifft2(I, m+1-mod(m, 2), n+1-mod(n, 2))));
+    R = abs(fftshift(ifft2(I)));
 
     [r, m0] = max(R);
     [~, n0] = max(r);
--- utils/IDFT_US.m
+++ utils/IDFT_US.m
@@ -1,39 +1,39 @@
 function offset = IDFT_US(im1, im2, k)
-%the IDFT_US algorithm
-%   k is the upsampling factor
+    %the IDFT_US algorithm
+    %   k is the upsampling factor
 
     [m, n] = size(im1);
     I1 = fft2(im1);
     I2 = fft2(im2);
-    
-    R = I1 .* conj(I2);
-    
-    r = abs(ifft2(fftshift(R)));
 
-    [l, m0] = max(r);
-    [~, n0] = max(l);
+    R = I1 .* conj(I2);
+    I = conj(R ./ abs(R));
+    r = abs(fftshift(ifft2(I)));
+
+    [r, m0] = max(r);
+    [~, n0] = max(r);
     m0 = m0(n0);
-    m0 = (m0 - 1);
-    n0 = (n0 - 1);
+    m0 = (m - mod(m, 2)) / 2 + 1 - m0;
+    n0 = (n - mod(n, 2)) / 2 + 1 - n0;
 
     M = fftshift(R);
-    u = 1: m;
-    v = 1: n;
+    u = 1:m;
+    v = 1:n;
     [u, v] = meshgrid(u, v);
     u = double(u');
     v = double(v');
-    L = exp(1j * 2 * pi *(u*m0/m + v*n0/n));
+    L = exp(1j * 2 * pi * (u * m0 / m + v * n0 / n));
     P = M .* L;
 
-    u = 1: m;
-    v = 1: n;
-    [U, V] = meshgrid(u, (-k: k) / k);
+    u = 1:m;
+    v = 1:n;
+    [U, V] = meshgrid(u, (-k:k) / k);
     U = U .* V;
     U_ = U;
-    [U, V] = meshgrid(v, (-k: k) / k);
+    [U, V] = meshgrid(v, (-k:k) / k);
     V = U .* V;
     V_ = V';
-    r_ = abs(exp(1j * 2 * pi * U_ / m) * P * exp(1j * 2* pi * V_ / n));
+    r_ = abs(exp(1j * 2 * pi * U_ / m) * P * exp(1j * 2 * pi * V_ / n));
 
     [l, x_] = max(r_);
     [~, y_] = max(l);
